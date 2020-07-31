d9c3b1d0459aabd676fe78f1d260379bcc1461a2
Your Name <legendofmiracles@protonmail.com>
2020-07-31 10:00:28 +0200

    added my fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+	thefuck --alias | source
+	starship init fish | source
+	figlet "Welcome Back!" | lolcat
+	cowsay -f tux "In a world without walls, who needs windows or gates?"| lolcat
+	printf "Log in time: "
+	date
+    
+end
+
+bind -k ppage 'xdotool key Up; sleep 0.1; xdotool keydown control key a; sleep 0.1; xdotool keyup control; xdotool type "sudo "'
+printf "%s\n" (date) >> /var/log/term_open

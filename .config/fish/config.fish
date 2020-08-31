1c1796b2eb2feff4d309884b7a89ced7ffcbcc75
ur name <legendofmiracles@protonmail.com>
2020-08-31 09:38:44 +0000

    some small tweaks here, some here

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,6 @@ if status is-interactive
 	cowsay -f tux "In a world without walls, who needs windows or gates?"| lolcat
 	printf "Log in time: "
 	date
-    navi widget fish | source
 end
 
 bind -k ppage 'xdotool key Up; sleep 0.1; xdotool keydown control key a; sleep 0.1; xdotool keyup control; xdotool type "sudo "'
@@ -14,3 +13,4 @@ printf "%s\n" (date) >> /var/log/term_open
 alias ls lsd
 alias mv "mv -v"
 alias emacs "i3-swallow emacs"
+alias doas "doas --"

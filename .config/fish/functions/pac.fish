f7a037ff9315c07451ce160ddd6df9388fb3cb1c
ur name <legendofmiracles@protonmail.com>
2020-09-29 04:37:53 +0000

    replacing sudo with yas in my dots

--- .config/fish/functions/pac.fish
+++ .config/fish/functions/pac.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function pac --wraps=pacman\ -Slq\ \|\ fzf\ --multi\ --preview\ \'pacman\ -Si\ \{1\}\'\ \|\ xargs\ -ro\ sudo\ pacman\ -S --description alias\ pac\ pacman\ -Slq\ \|\ fzf\ --multi\ --preview\ \'pacman\ -Si\ \{1\}\'\ \|\ xargs\ -ro\ sudo\ pacman\ -S
-  pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S $argv;
+  pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro yas pacman -S $argv;
 end

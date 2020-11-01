99674248f1ee0ef76294213bd4574f1de106bae3
ur name <legendofmiracles@protonmail.com>
2020-11-01 09:26:10 +0000

    hope

--- eww/egs.sh
+++ eww/egs.sh
@@ -1,8 +1,6 @@
 #!/bin/env sh
 #set -x
 echo '<box>'
-# with the command subsitition, does the xargs -I {} thing work? it seems like it, tho
-# this is a painful oneliner :(
 list=$(legendary list-installed --json | jq '.[].title' -r) # | xargs -n1 -p -d "\n" -I {} echo 'button onclick="'$(./app_name.sh {})'">'{}'</button>'
 while IFS= read -r line; do
 #    echo '<button onclick="legendary launch '$(./app_name.sh "$line")'">'$line'</button>'

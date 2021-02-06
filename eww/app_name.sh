cf188be5403aa6d5929343638545e4c024977148
legendofmiracles <legendofmiracles@protonmail.com>
2021-02-06 21:35:45 +0000

    eww

--- eww/app_name.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-#set -x
-legendary list-installed --json | jq 'map(if .title == "'"$1"'" then .app_name elif .title == "_" then "_" else "_" end) | map(select(. != "_")) | .[0]' -r

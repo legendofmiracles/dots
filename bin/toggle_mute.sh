6b1db275099149321e44952983ca06f17f6f6666
legendofmiracles <legendofmiracles@protonmail.com>
2021-03-09 17:56:40 +0000

    idun

--- bin/toggle_mute.sh
+++ bin/toggle_mute.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-set -x
-sources=$(pamixer --list-sources | /bin/grep -v monitor | /bin/grep -v Sources | cut -d " " -f 1 ) 
-
-while IFS= read -r line; do
-	case $(pamixer --get-volume-human --source $line) in
-		muted)
-			mplayer ~/unmuted.mp3 &
-			;;
-		*)
-			mplayer ~/muted.mp3 &
-			;;
-	esac
-done <<< "$sources"
-
-echo $sources | xargs -I {} pamixer --source {} -t
+pamixer --source 4 -t
